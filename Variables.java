7e2835ed0edb0cbbe67095a19c18f79999d81924
yusuf3a50 <ybyphone@disroot.org>
2022-10-06 17:32:42 +0100

    studying variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        char alphabetLetter = 'A';
+        double smallNumber = 0.9792880802809820;
+        int wholeNumber = 100;
+        String alphaNumericString = "this is a string with numbers 109801";
+        boolean isThisTheEnd = true;
+      System.out.println(alphabetLetter);
+      System.out.println(smallNumber);
+      System.out.println(wholeNumber);
+      System.out.println(alphaNumericString);
+      System.out.println(isThisTheEnd);
+    }
+}
