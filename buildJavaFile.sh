45162dd91710a5ef910eece56802e15d09e88f61
yusuf3a50 <ybyphone@disroot.org>
2022-10-06 14:37:34 +0100

    how to build java files

---
+++ buildJavaFile.sh
@@ -0,0 +1,4 @@
+#build the java file. this should create a new file named Filename.class
+javac Filename.java
+#run the build
+java Filename
---
+++ installJava.sh
@@ -0,0 +1,4 @@
+#install java
+sudo apt install openjdk-17-jdk
+#check which version of java is running
+java -version
